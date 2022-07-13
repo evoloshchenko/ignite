6324ebd311114b9cd1d953160237c19f7a0bce93
Jane Voloshchenko <j.voloshchenko@gmail.com>
2022-07-13 21:25:45 +0300

    A bit of animations for home page and search form

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,6 +6,7 @@ import { useDispatch } from "react-redux";
 import { loadDetail } from "../actions/detailAction";
 import { Link } from "react-router-dom";
 import { smallImage } from "../util";
+import { popup } from "../animations";
 
 const Game = ({ name, platforms, released, image, rating, genres, id }) => {
   const stringPathId = id.toString();
@@ -16,7 +17,13 @@ const Game = ({ name, platforms, released, image, rating, genres, id }) => {
     dispatch(loadDetail(id));
   };
   return (
-    <StyledGame layoutId={stringPathId} onClick={loadDetailHandler}>
+    <StyledGame
+      variants={popup}
+      initial="hidden"
+      animate="show"
+      layoutId={stringPathId}
+      onClick={loadDetailHandler}
+    >
       <Link to={`/game/${id}`}>
         <motion.img
           layoutId={`image ${stringPathId}`}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ import Game from "../components/Game";
 import styled from "styled-components";
 import { motion, AnimatePresence, LayoutGroup } from "framer-motion";
 import { useLocation } from "react-router-dom";
+import { fadeIn } from "../animations";
 
 const Home = () => {
   //Get the current Location
@@ -25,7 +26,7 @@ const Home = () => {
     (state) => state.games
   );
   return (
-    <GameList>
+    <GameList variants={fadeIn} initial="hidden" animate="show">
       <LayoutGroup type="crossfade">
         <AnimatePresence>
           {pathId && <GameDetail pathId={pathId} />}
